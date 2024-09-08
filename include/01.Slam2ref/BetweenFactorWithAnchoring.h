63dd179732b3aa70c98c0f77a7e2fd18e074eb6b
MigVega <mavtarturo@hotmail.com>
2024-09-08 14:43:58 +0200

    Refactor evaluateError signature in BetweenFactorWithAnchoring
    Update the function evaluateError to use gtsam::Matrix for optional Jacobians instead of Eigen::Matrix. This enhances consistency with other parts of the codebase and leverages gtsam's native matrix handling.

--- include/01.Slam2ref/BetweenFactorWithAnchoring.h
+++ include/01.Slam2ref/BetweenFactorWithAnchoring.h
@@ -107,15 +107,15 @@ namespace gtsam {
 
 
 /** vector of errors */
-virtual gtsam::Vector evaluateError(
-    const gtsam::Pose3& p1,
-    const gtsam::Pose3& p2,
-    const gtsam::Pose3& anchor_p1,
-    const gtsam::Pose3& anchor_p2,
-    boost::optional<Eigen::Matrix<double, -1, -1>&> H1 = boost::none,
-    boost::optional<Eigen::Matrix<double, -1, -1>&> H2 = boost::none,
-    boost::optional<Eigen::Matrix<double, -1, -1>&> anchor_H1 = boost::none,
-    boost::optional<Eigen::Matrix<double, -1, -1>&> anchor_H2 = boost::none
+gtsam::Vector evaluateError(
+        const gtsam::Pose3& p1,
+        const gtsam::Pose3& p2,
+        const gtsam::Pose3& anchor_p1,
+        const gtsam::Pose3& anchor_p2,
+        boost::optional<gtsam::Matrix&> H1 = boost::none,
+        boost::optional<gtsam::Matrix&> H2 = boost::none,
+        boost::optional<gtsam::Matrix&> anchor_H1 = boost::none,
+        boost::optional<gtsam::Matrix&> anchor_H2 = boost::none
 ) const override {
 
     // Step 1: Compute the composed poses with anchor nodes
